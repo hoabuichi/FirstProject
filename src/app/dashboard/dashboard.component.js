a74a46de6a2a948b5da31e0650e9caec24eaeddf
Hoa Bui Chi <buichihoa92@gmail.com>
2017-08-20 00:07:19 +0700

    finish card list

--- src/app/dashboard/dashboard.component.js
+++ src/app/dashboard/dashboard.component.js
@@ -23,6 +23,22 @@ var Cards = [
         unitIcon: 'fa-dollar',
         bGColor: '#16A085',
         moreInfoBG: '#149077'
+    },
+    {
+        icon: 'fa-bell',
+        title: 'alerts',
+        numberCard: 9,
+        unitIcon: 'fa-bell-o',
+        bGColor: '#F39C12',
+        moreInfoBG: '#DA8C10'
+    },
+    {
+        icon: 'fa-tasks',
+        title: 'tasks',
+        numberCard: 10,
+        unitIcon: 'fa-pie-chart',
+        bGColor: '#2980B9',
+        moreInfoBG: '#2573A6'
     }
 ];
 var DashboardComponent = (function () {
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -18,6 +18,22 @@ const Cards: StatisticCard[] = [
         unitIcon: 'fa-dollar',
         bGColor: '#16A085',
         moreInfoBG: '#149077'
+    },
+    {
+        icon: 'fa-bell',
+        title: 'alerts',
+        numberCard: 9,
+        unitIcon: 'fa-bell-o',
+        bGColor: '#F39C12',
+        moreInfoBG: '#DA8C10'
+    },
+    {
+        icon: 'fa-tasks',
+        title: 'tasks',
+        numberCard: 10,
+        unitIcon: 'fa-pie-chart',
+        bGColor: '#2980B9',
+        moreInfoBG: '#2573A6'
     }
 ];
 
